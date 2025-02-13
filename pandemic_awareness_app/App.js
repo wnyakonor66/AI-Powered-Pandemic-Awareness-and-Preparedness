5773cbc2949f0d99c17fe8ff3f505373313af655
wnyakonor66 <nyakonorwinfred6@gmail.com>
2025-02-13 03:13:28 +0000

    initial setup

---
+++ pandemic_awareness_app/App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { useState, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeStack from "./src/routes/HomeStack";
+
+SplashScreen.preventAutoHideAsync();
+
+const getFonts = async () =>
+  await Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-light": require("./assets/fonts/Nunito-Light.ttf"),
+  });
+
+export default function App() {
+  const [fontsloaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadResources() {
+      await getFonts();
+      setFontsLoaded(true);
+      await SplashScreen.hideAsync();
+    }
+    loadResources();
+  }, []);
+
+  if (!fontsloaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <HomeStack />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ pandemic_awareness_app/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { globalStyles } from "../../styles/global";
+
+const HomeScreen = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
